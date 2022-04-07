8ecb16c75415da5a14e2ef5da8ef7e30493a4cd6
Han Byul <yahb9@naver.com>
2022-04-07 23:58:30 +0900

    Feat: plant 이미지 추가 및 HospitalMap 컴포넌트 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,6 +3,7 @@ import {HashRouter, Route, Switch} from 'react-router-dom'
 import DetailPage from '../routes/DetailPage'
 import Home from '../routes/Home'
 import SearchResult from '../routes/SearchResult'
+import HospitalMap from '../routes/HospitalMap'
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                 <Route path={"/"} component={Home} exact></Route>
                 <Route path={"/result"} component={SearchResult} exact></Route>
                 <Route path={"/detail"} component={DetailPage} exact></Route>
+                <Route path={"/map"} component={HospitalMap} exact></Route>
             </Switch>
         </HashRouter>
     )
--- src/routes/DetailPage.tsx
+++ src/routes/DetailPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import {useRecoilValue} from 'recoil'
-import {docIdAtom} from '../components/Atom'
+import { useRecoilValue } from 'recoil'
+import { docIdAtom } from '../components/Atom'
 import axios from 'axios'
 import styled from "styled-components";
 import Top from "../components/Head";
@@ -26,7 +26,7 @@ margin-right: 15px;
 margin-top: 2.7rem;
 `
 
-const GoCommunity =styled(SearchHospital)``
+const GoCommunity = styled(SearchHospital)``
 
 const Form = styled.div`
 margin-left: auto;
@@ -42,14 +42,26 @@ const Title = styled.div``
 
 const Description = styled.div``
 interface Container {
-    num : number;
+    num: number;
 }
 const Text = styled.div<Container>`
 padding-top: ${(props) => (props.num % 2 === 0 ? '10px' : '30px')};
 font-size: ${(props) => (props.num % 2 === 0 ? '15px' : '20px')};
-color: ${(props) => (props.num % 2 === 0 ? 'black' : '#4598C7')};`
+color: ${(props) => (props.num % 2 === 0 ? 'black' : '#4598C7')};
+padding-left: 2rem;
+padding-right: 2rem;
+&:first-child {
+    padding-top: 3rem;
+}`
 
-function DetailPage () {
+const Plant = styled.div<Container>`
+background-image: url(${(props) => (props.num % 2 === 0 ? '' : `${process.env.PUBLIC_URL}/plant.png`)});
+background-size :contain;
+width: 23px;
+height: 27px;
+position: absolute;`
+
+function DetailPage(): JSX.Element {
     const docId = useRecoilValue(docIdAtom)
     const [docText, setDocText] = useState('')
 
@@ -62,30 +74,32 @@ function DetailPage () {
                     , converttitles: 1
                     , utf8: 1
                 }
-            }).then((response) => {setDocText(response.data.query.pages[`${docId}`].cirrusdoc[0].source.source_text); //split을 따로 빼야하나 고민 
-})
+            }).then((response) => {
+                setDocText(response.data.query.pages[`${docId}`].cirrusdoc[0].source.source_text); //split을 따로 빼야하나 고민 
+            })
     }
 
     useEffect(() => {
         searchWikiDocs()
     }, [])
     useEffect(() => {
-        if (docText !== ''){
-        console.log(docText.replace(/<ref>.{1,}\<\/ref\>/g, ''))
-    }
+        if (docText !== '') {
+            console.log(docText.replace(/\<ref.{0,}\>.+\<\/ref\>/g, '').replace(/\{\{.{0,}\}\}/g, '').replace(/\[\[/g, '').replace(/\]\]/g, '').replace(/\=\= 같이 보기 \=\=\n{0,}.{0,}/m, '').trim().replace(/\=\=\=/g, '=='))
+        }
     }, [docText])
 
     return <Body>
         <Top />
         <Button>
-        <SearchHospital>병원 찾기</SearchHospital>
-        <GoCommunity>커뮤니티</GoCommunity>
+            <SearchHospital>병원 찾기</SearchHospital>
+            <GoCommunity>커뮤니티</GoCommunity>
         </Button>
         <Form>
             <Title></Title>
             <Description>
                 {/* {docText.map((txt, idx)=> <Text key={txt} num={idx}>{txt}</Text>)} */}
-                {docText.replace(/<ref>.{1,}\<\/ref\>/g, '')}
+                {docText.replace(/\<ref.{0,}\>.+\<\/ref\>/g, '').replace(/\<ref.{0,}\>/g, '').replace(/\{\{.{0,}\}\}/g, '').replace(/\[\[/g, '').replace(/\]\]/g, '').replace(/\=\= 같이 보기 \=\=\n{0,}.{0,}/m, '').replace(/\=\= 각주 \=\=\n{0,}.{0,}/m, '').replace(/\=\= 외부 링크 \=\=\n{0,}.{0,}/m, '').trim().replace(/\=\=\=/g, '==').split(/\=\=/).map((txt, idx) => 
+                <><Plant num={idx} /><Text key={idx} num={idx}>{txt}</Text></>)}
             </Description>
         </Form>
     </Body>
